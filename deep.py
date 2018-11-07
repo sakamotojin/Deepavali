f69f21ccb2c6aff25c4b5a198bc2815481f32d00
Sumit Joshi <sumitjoshi_16146@aitpune.edu.in>
2018-11-07 08:01:01 -0500

    Update deep.py

--- deep.py
+++ deep.py
@@ -1,10 +1,17 @@
 
 import fbchat 
 from getpass import getpass 
-msg=fbchat.Message(text='HAPPY DEEPAVALI')
+
 username = str(input("Username: ")) 
 client = fbchat.Client(username, getpass()) 
 users = client.fetchAllUsers() 
-for i in users:   
-    sent = client.send(msg, thread_id=i.uid) 
+msg=fbchat.Message(text=input("Enter The Message You Want To Send : \t"))
+for i in users:
+    ss=  str("Uname : "+i.name +"\t ID :"+i.uid+'\n')
+    print ("Uname : "+i.name +" \tID :"+i.uid)
+    c=int(input("Send Msg To This User : y(1)/n(0)"))  
+    f = open("log.txt","a")
+    if(c==1) :
+	    f.write(ss+'\n'+msg+'\n\n')
+	    sent = client.send(msg, thread_id=i.uid) 
     
