9697ffa08a66ffc7973907163b86ac62ea68da6b
Sumit Joshi <sumitjoshi_16146@aitpune.edu.in>
2018-11-07 04:47:50 -0500

    Update deep.py

--- deep.py
+++ deep.py
@@ -1,1 +1,10 @@
 
+import fbchat 
+from getpass import getpass 
+msg=fbchat.Message(text='HAPPY DEEPAVALI')
+username = str(input("Username: ")) 
+client = fbchat.Client(username, getpass()) 
+users = client.fetchAllUsers() 
+for i in users:   
+    sent = client.send(msg, thread_id=i.uid) 
+    
